63ec84bc599ced59517d1c4b61431d6efd4f2b46
sham <sham@gmail.com>
2025-01-08 03:53:55 +0530

    learned about handling various http methods

--- src/app.js
+++ src/app.js
@@ -2,12 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",(req,res) => {
-    res.send("this is main page");
+app.get("/user",(req,res) => {
+    //get the data from data base
+    res.send("got the data of the users");
 })
 
-app.get("/user",(req,res) => {
-    res.send("i am handling user")
+app.get("/user/1",(req,res) => {
+    res.send("got the data from the first user");
+})
+
+app.post("/user",(req,res) => {
+    //post data
+    res.send("data for user posted sucessfully");
+})
+
+app.delete("/user",(req,res) => {
+    //post data
+    res.send("data for user deleted sucessfully");
+})
+
+app.put("/user",(req,res) => {
+    res.send("data is updated by replacement");
 })
 
 app.listen(3000,() => {
