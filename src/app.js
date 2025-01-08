bed9a09949f90a76648e9020e4e78926a4bcebfc
sham <sham@gmail.com>
2025-01-08 22:31:03 +0530

    wildcard error handling

--- src/app.js
+++ src/app.js
@@ -4,28 +4,15 @@ const {adminAuth,userAuth} = require("../utils/auth");
 const app = express();
 
 
-//only admin can perform these operations so we need to authenticate if the request is made by actual admin
-//this is done by using key
-//same was the reason why we used the key in tmdb project
-
-
-//instead of writing this auth logic twice we can write it in middleware to increase reusablity
-
-app.use("/admin",adminAuth);
-
-app.get("/user",userAuth,(req,res,next) => {
-    //logic to get all data of user
-    res.send("data of users");
+app.get("/user",(req,res,next) => {
+    throw new Error("fskhsfdfs");
+    res.send("user")
 })
 
-app.get("/admin/getdata",(req,res,next) => {
-    //logic to get all data
-    res.send("got all data");
-})
-
-app.delete("/admin/deletedata",(req,res,next) => {
-    //logic to delete the data
-    res.send("all data deleted")
+app.use("/",(err,req,res,next) => {
+    if(err){
+        res.status(500).send("unexpected error occured");
+    }
 })
 
 
