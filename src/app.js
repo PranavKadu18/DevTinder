5c0c1155e33f7ba749c2525a96f5c2351bcd77c0
sham <sham@gmail.com>
2025-01-08 16:01:09 +0530

    handling multiple route handlers for single route understanding execution with next()

--- src/app.js
+++ src/app.js
@@ -2,30 +2,49 @@ const express = require("express");
 
 const app = express();
 
-app.get(/.*man$/,(req,res) => {
-    //get the data from data base
-    res.send("got the data of the users");
-})
+app.get(/.*man$/, (req, res) => {
+  //get the data from data base
+  res.send("got the data of the users");
+});
 
-app.get("/user/:userid/:name",(req,res) => {
-    console.log(req.params);
-    res.send(req.params);
-})
+app.get("/user/:userid/:name", (req, res) => {
+  console.log(req.params);
+  res.send(req.params);
+});
 
-app.post("/user",(req,res) => {
+app.post(
+  "/user",
+  (req, res,next) => {
     //post data
-    res.send("data for user posted sucessfully");
-})
+    // res.send("data for user posted sucessfully");
+    next();
+  },
+  [(req, res, next) => {
+    console.log("post 2");
+    next();
+  },
+  (req,res,next) => {
+    console.log("post 3");
+    next();
+    res.send("done 3")
+    
+  }],
+  (req,res,next) => {
+    console.log("post 4");
+    // res.send("done 4")
+    // next();
+  }
+);
 
-app.delete("/user",(req,res) => {
-    //post data
-    res.send("data for user deleted sucessfully");
-})
+app.delete("/user", (req, res) => {
+  //post data
+  res.send("data for user deleted sucessfully");
+});
 
-app.put("/user",(req,res) => {
-    res.send("data is updated by replacement");
-})
+app.put("/user", (req, res) => {
+  res.send("data is updated by replacement");
+});
 
-app.listen(7777,() => {
-    console.log("connection established");
-})
+app.listen(3000, () => {
+  console.log("connection established");
+});
