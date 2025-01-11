4999714b9bfce2641a954d07a718bda4382fd002
sham <sham@gmail.com>
2025-01-11 08:12:22 +0530

    can send data dynamically using express.json middleware

--- src/app.js
+++ src/app.js
@@ -1,22 +1,14 @@
 const express = require("express");
-const {adminAuth,userAuth} = require("./Middlewares/auth");
 const {connect} = require("./Config/connect")
 const {User} = require("./models/user")
-const {Employe} = require("./models/employe");
-const { Student } = require("./models/student");
 
 const app = express();
 
-app.post("/signup",async (req,res) => {
-    const data = {
-        firstName : "Donald",
-        lastName : "Trump",
-        email : "donald@gmail.com",
-        password : "donald@123"
-    }
+app.use(express.json()); //middleware to convert JSON to JS object
 
+app.post("/signup",async (req,res) => {
     //we need to create the instance of model to add the data to db
-    const user = new User(data);
+    const user = new User(req.body);
 
     try {
         await user.save();
@@ -25,43 +17,6 @@ app.post("/signup",async (req,res) => {
         res.status(400).send("Error occured while sign up")
     }
 })
-
-app.post("/employe", async (req,res) => {
-    const data = {
-        name : "ravi",
-        role : "dba",
-        subject : "dbms"
-    }
-
-    const emp = new Employe(data);
-
-    try {
-        await emp.save();
-        res.send("Data saved (employe)")
-    } catch (error) {
-        res.status(400).send("failed");
-    }
-
-})
-
-app.post("/student",async (req,res) => {
-    const data = {
-        name : "Pranav",
-        rollno: 54,
-        class : "div1",
-        cgpa : 9.25
-    }
-
-    const stud = new Student(data);
-
-    try {
-        await stud.save();
-        res.send("data saved (student)");
-    } catch (error) {
-        res.status(400).send("error occured");
-    }
-})
-
 
 
 //first we should connect to db and then listen to any request
